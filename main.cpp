98e41c91bf23c84d3e654e700c02aab51cbb68ef
rlatjdgns <ericseonghunkim@gmail.com>
2026-04-03 14:33:52 -0700

    Day 3: Add printMessage function with const reference parameter

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include "include/message.h"
+void printMessage(const Message& m){
+    m.print();
+}
+
 
 int main(){
     Message m(1,2,3,MessageType::TEMPERATURE,23.3);
-    m.print();
+    printMessage(m);
     return 0;
 }
