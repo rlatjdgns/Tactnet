6132753c82c41b1ccea859bc66aa5dd4622c3228
rlatjdgns <ericseonghunkim@gmail.com>
2026-04-13 21:45:53 -0700

    Day 9: Test LoRaDriver in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include "include/node.h"
 #include "include/scheduler.h"
+#include "include/LoRaDriver.h"
 #include <iostream>
 #include <string>
+
 void printMessage(const Message& m){
     m.print();
 }
@@ -16,23 +18,31 @@ int main(){
 
     */
     
-    Node n1 = Node(1);
-    Node n2 = Node(2);
-    Node n3 = Node(3);
+    // Node n1 = Node(1);
+    // Node n2 = Node(2);
+    // Node n3 = Node(3);
 
-    n1.add_neighbor(&n2); n1.add_neighbor(&n3);
-    n2.add_neighbor(&n1); n2.add_neighbor(&n3);
-    n3.add_neighbor(&n1); n3.add_neighbor(&n2);
+    // n1.add_neighbor(&n2); n1.add_neighbor(&n3);
+    // n2.add_neighbor(&n1); n2.add_neighbor(&n3);
+    // n3.add_neighbor(&n1); n3.add_neighbor(&n2);
 
-    Task t1 = Task(TaskType::SEND_TEMPERATURE, 3, 10);
-    Task t2 = Task(TaskType::SEND_STATUS, 2, 5);
-    Task t3 = Task(TaskType::SEND_ERROR, 1, 2);
+    // Task t1 = Task(TaskType::SEND_TEMPERATURE, 3, 10);
+    // Task t2 = Task(TaskType::SEND_STATUS, 2, 5);
+    // Task t3 = Task(TaskType::SEND_ERROR, 1, 2);
     
-    n1.add_task(t1);n1.add_task(t2); n1.add_task(t3);
-    n1.run_task();
+    // n1.add_task(t1);n1.add_task(t2); n1.add_task(t3);
+    // n1.run_task();
 
-    n1.print_node(); n2.print_node(); n3.print_node();
+    // n1.print_node(); n2.print_node(); n3.print_node();
         
+
+
+    LoRaDriver LD1 = LoRaDriver(1);
+    LD1.begin();
+    LD1.send(2,"Hello");
+    std::cout<<LD1.recieve()<<"\n";
+    LD1.disconnect();
+
     return 0;
 }
 
