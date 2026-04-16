16b6350cb2f793905933c9de2c57c9ffefb228a1
rlatjdgns <ericseonghunkim@gmail.com>
2026-04-16 16:17:32 -0700

    Fix serial timeout

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "include/node.h"
 #include "include/scheduler.h"
 #include "include/LoRaDriver.h"
+#include "include/BME280Driver.h"
 #include <iostream>
 #include <string>
 #include <cstdlib>
@@ -14,6 +15,12 @@ int main(int argc, char* argv[]){
     int nodeID = std::atoi(argv[1]); 
     
     if(nodeID == 1){
+        BME280Driver bme;
+        bme.begin();
+        SensorReadings readings = bme.read();
+        std::cout << "Temperature: " << readings.temperature << "C\n";
+        std::cout << "Humidity: " << readings.humidity << "%\n";
+        std::cout << "Pressure: " << readings.pressure << " hPa\n";
         Node n1(1);
         n1.add_neighbor(2);
         Message m(1, 2, 1, MessageType::TEMPERATURE, 23.5);
