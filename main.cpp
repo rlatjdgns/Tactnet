7c657a32291c69b04e60e111c54c5148ddb7d4f1
rlatjdgns <ericseonghunkim@gmail.com>
2026-04-17 13:08:39 -0700

    Day 12: Update Message payload to string, fix scheduler

--- main.cpp
+++ main.cpp
@@ -37,8 +37,7 @@ int main(int argc, char* argv[]){
         std::cout << "Node 2 listening...\n";
         while(true){
             if(n2.receive()){
-                std::cout << "|Message received from "<<n2.messages[message_count].senderID<<"|\n";
-                std::cout << "------------------------------"
+                std::cout << "Message received!\n";
                 n2.print_node();
             }
         }
