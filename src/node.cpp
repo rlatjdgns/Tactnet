679aa838ef113e96d896a5f3e22fd6ecb0b5e1c7
rlatjdgns <ericseonghunkim@gmail.com>
2026-04-22 10:59:24 -0700

    Day 13: Timestamp failover verified on all nodes

--- src/node.cpp
+++ src/node.cpp
@@ -40,7 +40,6 @@ bool Node::receive(){
         while(std::getline(ss, token, '|')){
         tokens.push_back(token);
         }
-        std::cout << "tokens[1]: " << tokens[1] << " node_ID: " << node_ID << "\n";
         if(std::stoi(tokens[1])==node_ID){ 
             Message m = Message(msg.senderAddress, this->node_ID, this->message_count, MessageType::SENSORREADING, tokens[4].substr(9) + "|" + tokens[5] + "|" + tokens[6]);
             std::cout << "t4: " << tokens[4] << " t5: " << tokens[5] << " t6: " << tokens[6] << "\n";
