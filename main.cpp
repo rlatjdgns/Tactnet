762ebb42fcf26b781af4d9c9ecac3358f6d34362
rlatjdgns <ericseonghunkim@gmail.com>
2026-04-26 15:07:02 -0700

    Day 14: Integrate scheduler with BME280, Node owns sensor

--- main.cpp
+++ main.cpp
@@ -18,19 +18,13 @@ int main(int argc, char* argv[]){
     if(nodeID == 1){
         Node n1(1);
         n1.add_neighbor(2);
-        n1.add_task(Task(TaskType::SEND_READINGS, 1, 5));
+        n1.add_task(Task(TaskType::SEND_READINGS, 1, 10));
 
         while(true){
             n1.run_task();
             if(n1.receive()){
                 n1.print_node();
             }
-
-            // for(int i=0;i<5;i++){
-            //     if(n1.receive()){
-            //         n1.print_node();
-            //     }
-            // }
             n1.check_neighbors();
         }
     }
@@ -39,18 +33,15 @@ int main(int argc, char* argv[]){
         Node n2(2);    
         n2.add_neighbor(1);
         n2.add_neighbor(3);
+        n2.add_task(Task(TaskType::SEND_READINGS,1,20));
         std::cout << "Node 2 listening...\n";
         std::cout<<"-----------------------------------\n";
         while(true){
-            n2.check_neighbors();
             if(n2.receive()){
                 n2.print_node();
-                SensorReadings r = n2.read_sensor();
-                std::string ping_payload = std::to_string(r.temperature) + "|" + std::to_string(r.humidity) + "|" + std::to_string(r.pressure);
-                std::cout<< ping_payload<<"\n";
-                Message m(2, 1, 1, MessageType::STATUS_PING, ping_payload);
-                n2.send_to(1, m);
-            }   
+            }
+            n2.run_task();   
+            n2.check_neighbors();
         }
     }
     
