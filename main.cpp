9e88cc954ec076ec4316886918c495713964c062
rlatjdgns <ericseonghunkim@gmail.com>
2026-04-26 14:37:25 -0700

    Day 14: Integrate scheduler with BME280, Node owns sensor

--- main.cpp
+++ main.cpp
@@ -22,11 +22,11 @@ int main(int argc, char* argv[]){
 
         while(true){
             n1.run_task();
-            for(int i=0;i<5;i++){
-                if(n1.receive()){
-                    n1.print_node();
-                }
-            }
+            // for(int i=0;i<5;i++){
+            //     if(n1.receive()){
+            //         n1.print_node();
+            //     }
+            // }
             n1.check_neighbors();
         }
     }
