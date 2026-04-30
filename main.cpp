a29f6402d5c181ac19ed024c9d442a8e9fcb7671
rlatjdgns <ericseonghunkim@gmail.com>
2026-04-30 12:39:29 -0700

    Code cleanup: consistent formatting, comments, helper functions

--- main.cpp
+++ main.cpp
@@ -27,7 +27,7 @@ int main(int argc, char* argv[]) {
 
         while (true) {
             n1.run_task();
-            for(int i = 0; i < 5; i++){
+            for(int i = 0; i <10; i++){
                 if (n1.receive()) n1.print_node();
                 ::usleep(200000); 
             }
