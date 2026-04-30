1513d2823fc97045fd0599fae9069bf7843388bb
rlatjdgns <ericseonghunkim@gmail.com>
2026-04-29 22:56:36 -0700

    Code cleanup: consistent formatting, comments, helper functions

--- src/node.cpp
+++ src/node.cpp
@@ -64,7 +64,8 @@ bool Node::receive() {
         receive_message(m);
         for (int i = 0; i < neighbor_count; i++) {
             if (neighbor_addresses[i] == destinationID) {
-                lora.send(neighbor_addresses[i], msg.payload);
+                bool sent = lora.send(neighbor_addresses[i], msg.payload);
+                std::cout<< "result: "<<sent<<"\n"; 
             }
         }
         return true;
