02d82a72b5a64c64dc69a239b16fa42dee70e970
rlatjdgns <ericseonghunkim@gmail.com>
2026-05-04 21:33:14 -0700

    Final: Live Dashboard

--- main.cpp
+++ main.cpp
@@ -37,11 +37,11 @@ int main(int argc, char* argv[]) {
                     Message latest = n1.get_latest_message();
                     if (latest.get_sender_ID() == 2) {
                         node2_payload = latest.get_payload();
-                        if (latest.get_msg_type() == MessageType::ERROR) alert = "Node 2 ALERT";
+                        if (latest.get_message_type() == MessageType::ERROR) alert = "Node 2 ALERT";
                     }
                     if (latest.get_sender_ID() == 3) {
                         node3_payload = latest.get_payload();
-                        if (latest.get_msg_type() == MessageType::ERROR) alert = "Node 3 ALERT";
+                        if (latest.get_messsage_type() == MessageType::ERROR) alert = "Node 3 ALERT";
                     }
 
                     SensorReadings r1 = n1.read_sensor();
